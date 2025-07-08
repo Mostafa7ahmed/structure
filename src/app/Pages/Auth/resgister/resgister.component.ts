764a968016021c2204f8f12456383dc763af79e7
mostafa hamed <Mostafa7ahmed>
2025-07-08 11:21:45 +0300

    structure angular

---
+++ src/app/Pages/Auth/resgister/resgister.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgister',
+  standalone: true,
+  imports: [],
+  templateUrl: './resgister.component.html',
+  styleUrl: './resgister.component.scss'
+})
+export class ResgisterComponent {
+
+}
